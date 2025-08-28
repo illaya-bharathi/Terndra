c71cff22fae208e9a5863d6bc66a06aa8d5a920e
Illaya Bharathi <illayabharathi1112@gmail.com>
2025-08-28 21:53:21 +0530

    eight

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,80 +1,78 @@
-import userModel from "../models/userModels.js";
-import bycrpt from "bcrypt"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
-const createToken = (id) =>{
-    return jwt.sign({id},process.env.JWT_SECRET)
-}
+const createToken = (userId) => {
+  const payload = { id: userId };
+  return jwt.sign(payload, process.env.JWT_SECRET);
+};
 
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-const registerUser = async(req,res)=>{
-    try {
-        const {name,email,password,} = req.body
-
-        const exits = await userModel.findOne({email})
-
-        if(exits) { 
-            return res.json({suceess:false,message:"User already exits"})
-        }
-
-        // validating email format & strong password4
-
-        if(!name || !email || !password){
-        return res.json({suceess:false,message:'Fill all the fileds'})
-
-        }
-        
-        if(password.length < 8) {
-            return res.json({success:false,message:'Please enter a valid email'})
-        }
-
-        const salt = await bycrpt.genSalt(10)
-        const hasedPassword = await bycrpt.hash(password,salt)
-
-        const newUser = new userModel({
-            name,
-            email,
-            password:hasedPassword
-        })
-
-        const user = await newUser.save()
-
-        const token = createToken(user._id.toString())
-
-        res.json({suceess:true,token})
-    } catch (error) {
-        console.log(err)
-          res.json({success:false,message:err.message})
+    // check existing user
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
     }
-}
 
-const loginUser = async(req,res)=>{
-    try {
-        const {email,password} = req.body;
-
-        const user = await userModel.findOne({email})
-
-        if(!user){
-            return res.json({suscess:false,message:'User already exists'})
-        }
-
-        const isMatch = await bycrpt.compare(password,user.password)
-
-        if (isMatch) {
-            const token = createToken(user.id.toString())
-            res.json({success:true,token})
-        }else{
-            res.json({success:false,message:'Inavlid credentails'}) 
-        }
-    } catch (error) {
-         console.log(error)
-     res.json({success:false,message:error.message})
+    // validate fields
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Fill all the fields" });
     }
-}
 
+    // validate password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password must be at least 8 characters",
+      });
+    }
 
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
+    const user = await newUser.save();
 
+    const token = createToken(user._id.toString());
 
-export {loginUser,registerUser}
+    res.json({ success: true, token });
+  } catch (error) {
+    console.error("Register Error:", error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = createToken(user._id.toString());
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error("Login Error:", error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { loginUser, registerUser };
